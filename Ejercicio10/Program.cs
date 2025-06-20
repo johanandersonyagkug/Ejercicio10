f5fd09188a1d2f6b079195140f2f07ba8e3f33b3
johan <77202059@idexperujapon.edu.pe>
2025-06-20 18:32:48 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, suma ;
+
+            Console.WriteLine("Introduce un números");
+            numero = Convert.ToInt32(Console.ReadLine());
+            suma = numero;
+            while (numero != 0)
+            {
+                Console.WriteLine("Introduce un números");
+                numero = Convert.ToInt32(Console.ReadLine());
+                suma = suma + numero;
+            }
+
+            Console.WriteLine("La suma es:{0} " ,suma);
+           
+        }
+    }
+}   
+
+
+    
+
